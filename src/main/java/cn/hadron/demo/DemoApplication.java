93e5b46e41d54b52e0d3e76d1800dba83832398d
zgl <361251205@qq.com>
2019-08-15 00:40:01 +0800

    Initial commit

---
+++ src/main/java/cn/hadron/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package cn.hadron.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@Component
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
