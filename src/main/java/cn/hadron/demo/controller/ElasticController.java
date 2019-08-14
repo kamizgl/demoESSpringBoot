1dfd5e58a8e96425bb7b586287fa21ad0f6d00a9
zgl <361251205@qq.com>
2019-08-15 01:02:40 +0800

    增加query

--- src/main/java/cn/hadron/demo/controller/ElasticController.java
+++ src/main/java/cn/hadron/demo/controller/ElasticController.java
@@ -1,8 +1,12 @@
 package cn.hadron.demo.controller;
 
 import cn.hadron.demo.bean.BookBean;
+import cn.hadron.demo.dao.BookRepository;
 import cn.hadron.demo.service.BookService;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +17,8 @@ import java.util.Optional;
 public class ElasticController {
     @Autowired
     private BookService bookService;
-
+    @Autowired
+    private BookRepository  bookRepository;
     @RequestMapping("/book/{id}")
     @ResponseBody
     public BookBean getBookById(@PathVariable String id){
@@ -30,5 +35,21 @@ public class ElasticController {
         System.out.println(book);
         bookService.save(book);
     }
+
+
+    @RequestMapping("/query")
+    @ResponseBody
+    public void query(){
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("title","ES入门教程"));
+        Page<BookBean> search = bookRepository.search(nativeSearchQueryBuilder.build());
+
+
+//        BookBean book=new BookBean("1","ES入门教程","程裕强","2018-10-01");
+//        System.out.println(book);
+//        bookService.save(book);
+        System.out.println(search);
+    }
+
 
 }
